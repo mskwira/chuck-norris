eb061b3471816cc8f64b8b9d921982d53f7a34d7
Michał Skwira <michalskwira@yahoo.com>
2019-09-17 12:34:36 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,26 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'http://apis.is/concerts', false); // async flag set to false
+request.send(); // interface suspension
+if(request.status == 200) {
+  console.log(request.response);
+}
+
+var url = 'http://api.icndb.com/jokes/random';
+
+var button = document.getElementById('get-joke');
+	button.addEventListener('click', function(){
+  	getJoke();
+	});
+
+var paragraph = document.getElementById('joke');
+	function getJoke() {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.addEventListener('load', function(){
+    	var response = JSON.parse(xhr.response);
+    	paragraph.innerHTML = response.value.joke;
+  	});
+  
+    xhr.send();
+var response = JSON.parse(xhr.response);
+  };
